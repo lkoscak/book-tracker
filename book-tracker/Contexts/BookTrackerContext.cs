c39a11747e3d6d0abaef0d8c563a56c157ef94a4
unknown <luka.koscak96@gmail.com>
2020-03-05 12:12:32 +0100

    Created Context class, configured app to work with database context. Added Book model class

---
+++ book-tracker/Contexts/BookTrackerContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace book_tracker.Contexts
+{
+    public class BookTrackerContext : DbContext
+    {
+        public BookTrackerContext(DbContextOptions<BookTrackerContext> options) : base(options) { }
+    }
+}
--- book-tracker/Startup.cs
+++ book-tracker/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using book_tracker.Contexts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,8 @@ namespace book_tracker
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<BookTrackerContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("SQLServerConnectionString")));
             services.AddControllersWithViews();
         }
 
